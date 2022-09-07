bc997cb2080312f290f8f17cb71c59249f3c9f4c
Joe O'Donohoe <joeodonohoe@live.com>
2022-09-07 20:30:07 +0000

    admin panel updated with comment management

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'post_create_date')
+    list_display = ('title', 'slug', 'status', 'post_create_date')
+    search_fields = ('title', 'post_content')
     summernote_fields = ('post_content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
