e2d61f7cb17ba2685e4b933ae51379958b5a0634
Joe O'Donohoe <joeodonohoe@live.com>
2022-09-12 19:25:58 +0000

    finish post detail page and link to allauth

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 from .models import Comment
 from .models import Poll
@@ -11,3 +11,89 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-post_create_date')
     template_name = 'index.html'
     paginate_by = 4
+
+"""
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('post_create_date')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+"""
+
+"""
+def post_detail(request, slug, *args, **kwargs):
+
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved = True).count()
+    liked = False
+    commented = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
+
+    if request.method == "POST":
+    
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment awaiting moderation.')
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        "post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "liked": liked,
+            "comment_form": comment_form
+        },
+    )
+"""
+def post_detail(request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved = True).count()
+    liked = False
+    commented = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
+
+
+    return render(
+        request,
+        "post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "liked": liked
+        },
+    )
